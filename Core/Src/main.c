ce406b9f5d1d1c676228a8d8101392d8fd985d1e
Eric Flynn <eflynn7778@gmail.com>
2025-03-22 19:40:47 -0400

    Update main.c for HID Bootloader
    Update main.c for HID Bootloader

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -83,7 +83,7 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
+  SCB->VTOR = FLASH_BASE | 0x4000; // Added for HID bootloader offset
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
